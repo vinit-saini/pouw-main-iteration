67796590b3775b9c3f72eafed4034570d32ae0df
vinit saini <saini.vinit@hotmail.com>
2024-06-09 18:26:36 +0000

    proxy config

--- pai/pouw/mining/gbtminer.py
+++ pai/pouw/mining/gbtminer.py
@@ -55,11 +55,7 @@ class Miner:
     @property
     def _rpc_connection(self):
         print(f'_rpc_connection: _rpc_user={self._rpc_user} rpc_password={self._rpc_password} server_ip={self._server_ip} server_port={self._server_port}')
-        proxyConfig = AuthServiceProxy("http://%s:%s@%s:%d"
-                                % (self._rpc_user, self._rpc_password,
-                                self._server_ip, self._server_port))
-        print(f'proxyConfig: {proxyConfig}')
-        return proxyConfig
+        return AuthServiceProxy("http://%s:%s@%s:%d" % (self._rpc_user, self._rpc_password, self._server_ip, self._server_port))
 
     @staticmethod
     def _check_nonce(blkhash, target):
